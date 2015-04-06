13260f189d46394f7bdafd26f6dce440b7ebdf71
Saori Kohno <SaoKKK@users.noreply.github.com>
2015-04-06 14:27:12 +0900

    Initial Commit

---
+++ TagController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TagController
+//
+//  Created by 河野 さおり on 2015/04/06.
+//  Copyright (c) 2015年 Saori Kohno. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TagController/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TagController
+//
+//  Created by 河野 さおり on 2015/04/06.
+//  Copyright (c) 2015年 Saori Kohno. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
